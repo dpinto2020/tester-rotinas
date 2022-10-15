863aed009cd05354e6f6f050ba3ff50dffe7d6f4
dpinto2020 <dpinto.diego@gmail.com>
2022-10-15 18:47:25 -0300

    Iniciando o projeto tester

---
+++ tester-rotinas/src/br/com/rotinas/implement/crud/VariavelConexaoUtil.java
@@ -0,0 +1,7 @@
+package br.com.rotinas.implement.crud;
+
+//Nome do caminho do JNDI datasource Tomcat
+public class VariavelConexaoUtil {
+
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+}
